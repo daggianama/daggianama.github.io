99acf39904874cfe6bb905dd37b7dda394461d9d
Daggiana Madrid Andrade <daggianama@gmail.com>
2023-07-04 12:40:18 +0200

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,25 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
-      <h1>Daggiana Madrid</h1>
-        { <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-200  from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        <a href="www.daggianamadrid.com" target="_blank"
-            >UX/UI projects</a>
-        </p> }
-        <div className="left-0 flex h-48 w-full justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main className="flex min-h-screen flex-col items-start  py-20">
+      <div className="flex flex-col z-10  max-w-8xl mx-32 items-start justify-between font-mono text-sm">
+        <h1 className='mb-28 text-lg'>Daggiana Madrid</h1>
+        
+        <a href="https://daggianamadrid.com" target="_blank" rel="noopener noreferrer"
+           className="w-40 justify-center border-white  my-2 backdrop-blur-2xl    lg:rounded-xl lg:border lg:p-3 hover:bg-white" >UX/UI projects</a>
+        
        
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="w-40 justify-center border-white  my-2 backdrop-blur-2xl  lg:rounded-xl lg:border lg:p-3 hover:bg-white"
+            href="https://github.com/daggianama"
             target="_blank"
             rel="noopener noreferrer"
           >
           Github
           </a>
-        </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+      <div className="flex absolute bottom-2 right-80 before:h-[280px] before:w-[400px] before:-translate-x-1/3 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-xl before:content-[''] after:absolute after:-z-20 after:h-[200px] after:w-[400px] after:translate-x-1/3 after:bg-gradient-conic after:from-red-200 after:via-orange-100 after:blur-2xl after:content-['']  before:lg:h-[300px]">
         
         
       </div>
