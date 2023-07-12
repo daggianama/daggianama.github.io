a3fceed9dddcd9b2d32898e2c571151150a7b356
Daggiana Madrid Andrade <daggianama@gmail.com>
2023-07-13 01:56:17 +0200

    small copy change

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,18 @@
     <main class="flex min-h-screen max-w-md pt-8 flex-col items-start py-8 md:max-w-2xl md:ml-28 md:pt-20" >
       
       <div class="flex flex-col gap-1 md:gap-2 z-10  text-gray-800 px-6 w-sm  items-start justify-between ">
-        <div class="flex flex-wrap md:flex md:gap-6 items-baseline   ">
+        <div class="flex flex-wrap gap-1 md:flex md:gap-6 items-baseline   ">
           <h1 class="text-2xl font-mono ">Daggiana Madrid</h1>
           <a class="text-main-blue text-sm" href="mailto:daggianama@gmail.com">daggianama@gmail.com</a>
         </div>
-        <div class="flex font-light flex-col gap-3 py-2  text-gray-600 text-sm mb-2 ">
+        <div class="flex font-light flex-col gap-2 py-2 max-w-lg text-gray-600 text-sm mb-2 ">
           <p>Developer and artist based in Barcelona with a diverse background in creative topics: visual arts, 3d printing, video-art, research and design.
             </p>
             <p>Around 10 years old, I learned about coding with <a className="text-main-blue/90 " href="https://el.media.mit.edu/logo-foundation/what_is_logo/logo_primer.html" target="_blank"
               rel="noopener noreferrer">Logo language</a>, which led me to my fascination for problem-solving and building by experimenting with computer programs.</p>
-          <p> Some of my interests are Creative development, Human-computer Interaction, Information technology, Computational ethics, among others topics related humans and technology.</p>
+          <p> Some of my interests are Creative development, Human-computer Interaction, Data science, Computational ethics, among others topics related humans and technology.</p>
         </div>
-        <div class="flex flex-wrap md:flex-nowrap  ml-0 text-sm md:w-full items-center gap-2 mt-6">
+        <div class="flex flex-wrap md:flex-nowrap mt-2 ml-0 text-sm md:w-full items-center gap-2 md:mt-10">
           <a
               class="md:w-auto w-1/2 bg-white/20 text-center   backdrop-blur-2xl border border-black/50   p-1 md:p-2 md:px-4 md:hover:bg-white/40 md:hover:border md:hover:border-white/60"
               href="https://github.com/daggianama"
