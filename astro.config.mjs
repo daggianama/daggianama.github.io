573696909e666947ec4696ff671be4641900de92
Daggiana Madrid <daggianama@gmail.com>
2024-11-03 13:10:06 +0100

    formatting  + cleaning

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
-import { defineConfig } from "astro/config";
-import vue from "@astrojs/vue";
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-  projectRoot: "./",
-  public: "./public/",
-  dist: "./dist/",
-  src: "./src/",
+  projectRoot: './',
+  public: './public/',
+  dist: './dist/',
+  src: './src/',
   renderers: ['@astrojs/renderer-vue'],
-  integrations: [vue()]
+  integrations: [vue()],
+  vite: {
+    resolve: {
+      alias: {
+        '@/': path.resolve('./src'),
+      },
+    },
+  },
 });
