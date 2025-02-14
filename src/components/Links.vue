f3fbdd993a56edce0b0a776399ca6fc6480aed3b
Daggiana Madrid <daggianama@gmail.com>
2025-02-14 20:45:13 +0100

    fixing unsolved links git config

---
+++ src/components/Links.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="links-box text-main-blue p-4 w-sm items-start z-20 mt-2 bg-white/80 border border-main-blue/30"
+  >
+    <ul class="flex flex-col gap-4 font-mono font-light">
+      <li>
+        <a
+          href="https://www.johnseelybrown.com/calmtech.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          >· Calm Tech, Then and Now.</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://arena-attachments.s3.amazonaws.com/11968748/f25103711b83a16b87b1e346a0927333.pdf?1621201053"
+          target="_blank"
+          rel="noopener noreferrer"
+          >· The coming age of calm technology.</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://projects.kwon.nyc/internet-is-fun/"
+          target="_blank"
+          title="texts & articles related internet nostalgia"
+          rel="noopener noreferrer"
+          >· The internet used to be ✨fun✨</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://softglossary.space/"
+          target="_blank"
+          title="tools and stuff for junior web devs"
+          rel="noopener noreferrer"
+          >· Soft space for web design & code</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://ptgmedia.pearsoncmg.com/images/9780321918628/samplepages/0321918622.pdf"
+          target="_blank"
+          title="pdf book about computers"
+          rel="noopener noreferrer"
+        >
+          · Computers as Theater.
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import './Links.css';
+
+export default {
+  name: 'Links',
+};
+</script>
